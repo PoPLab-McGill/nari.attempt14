f80981f3703c3beb13faf0b3989b4d4e43f1e90b
Psychology of Pragmatics Lab <poplab.scsd@gmail.com>
2025-01-21 11:40:48 -0500

    Update trial.js

--- trial.js
+++ trial.js
@@ -1,22 +1,24 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/PoPLab-McGill/nari.attempt12@main/extension.js'], function(APIConstructor, stiatExtension){
-    var API = new APIConstructor();
-
-    return stiatExtension({
-        remindErrorText: '<p align="center" style="font-size:0.6em; font-family:arial;">' +
-            'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
-            'Press the other key to continue.<p/>',
-        category: {
-            name: 'Autistics',
-            title: {media: {word: 'Autistics'}, css: {color: '#31b404', 'font-size': '2em'}, height: 7},
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/PoPLab-McGill/nari.attempt13@main/extension.js'], function(APIConstructor, stiatExtension){
+var API = new APIConstructor();
+		  return stiatExtension({
+		  category : { 
+		    name : 'Autistics', //Will appear in the data.
+		    title : {
+		      media : {word : 'Black people'}, //Name of the category presented in the task.
+		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+		      height : 7 //Used to position the "Or" in the combined block.
+		    }, 
             media: [
-  { word: 'Avoids eye contact' },
+                    { word: 'Avoids eye contact' },
                     { word: 'Dislikes unexpected changes' },
                     { word: 'Struggles making friends' },
                     { word: 'Repeats certain words' }
             ],
             css: {color: '#31b404', 'font-size': '3em'}
-        },
-        attribute1: {
+		  },	
+
+  		attribute1 : 
+			{
             name: 'Negative',
             title: {media: {word: 'Negative'}, css: {color: '#31b404', 'font-size': '2em'}, height: 7},
             media: [
@@ -24,9 +26,10 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/PoPLab-McGill/nari.attempt12@main
                     { word: 'Angry' },
                     { word: 'Hate' },
                     { word: 'Evil' }
-            ],
-            css: {color: '#31b404', 'font-size': '3em'}
-        },
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'3em'}
+			},
         attribute2: {
             name: 'Positive',
             title: {media: {word: 'Positive'}, css: {color: '#31b404', 'font-size': '2em'}, height: 7},
