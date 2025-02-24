dfc34104de524bacf5faf07de4ff7816ee901aa0
Psychology of Pragmatics Lab <poplab.scsd@gmail.com>
2025-02-24 19:56:45 +0100

    Update fextension.js

--- fextension.js
+++ fextension.js
@@ -69,23 +69,15 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 					instHTML : '', //Empty means we will create the inst from the instTemplate variable further below. 
 					block : 1, //The block variable is not used later, but could help the user. 
 					//In each block, we can include a number of mini-blocks, to reduce repetition of same group/response.
-					miniBlocks : 1, //Set to 1 if don't need mini blocks. 0 will break the task.
+					miniBlocks : 2, //Set to 1 if don't need mini blocks. 0 will break the task.
 					singleAttTrials : 10, //Number of trials of the attribute that does not share key with the category (in a mini block).
-					sharedAttTrials : 10, //Number of trials of the attribute that shares key with the category (in a mini block).
-					categoryTrials : 0 // Number of trials of the category (in a mini-block). If 0, the label does not appear.
+					sharedAttTrials : 7, //Number of trials of the attribute that shares key with the category (in a mini block).
+					categoryTrials : 7 // Number of trials of the category (in a mini-block). If 0, the label does not appear.
 					//Note: if no category trials, then attribute1, the one on the left, is considered the single attribute.
 				}, 
 				{ 
 					instHTML : '', 
 					block : 2, 
-					miniBlocks : 2, 
-					singleAttTrials : 10, 
-					sharedAttTrials : 7, 
-					categoryTrials : 7
-				}, 
-				{ 
-					instHTML : '', 
-					block : 3, 
 					miniBlocks : 2, 
 					singleAttTrials : 16, 
 					sharedAttTrials : 10, 
@@ -93,7 +85,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				}, 
 				{ 
 					instHTML : '', 
-					block : 4, 
+					block : 3, 
 					miniBlocks : 2, 
 					singleAttTrials : 10, 
 					sharedAttTrials : 7, 
@@ -101,7 +93,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				}, 
 				{ 
 					instHTML : '', 
-					block : 5, 
+					block : 4, 
 					miniBlocks : 2, 
 					singleAttTrials : 16, 
 					sharedAttTrials : 10, 
@@ -110,7 +102,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			],
 			//All blocks show attribute1 on the left and attribute2 on the right. 
 			//blockOrder can be: 'startRight', 'startLeft', and 'random'
-			blockOrder : 'startRight', 
+			blockOrder : 'startLeft', 
 			//Change to 'startRight' if you want to start with category on the right in the first block. 
 			//Change to 'startLeft' if you want to start with category on the left in the first block. 
 			//Change to 'random' if you want to randomize whether the category starts on the left or on the right.
