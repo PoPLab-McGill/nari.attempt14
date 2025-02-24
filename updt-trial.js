9ba03fcb293690c022be6d58e0a09ea8586cd683
Psychology of Pragmatics Lab <poplab.scsd@gmail.com>
2025-02-24 20:27:45 +0100

    Update and rename updated-trial.js to updt-trial.js

--- updt-trial.js
+++ updt-trial.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/PoPLab-McGill/smiaa-sc-iat@main/updated-extension.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/PoPLab-McGill/smiaa-sc-iat@main/updt-extension.js'], function(APIConstructor, stiatExtension){
 var API = new APIConstructor();
 		  return stiatExtension({
 		  category : { 
