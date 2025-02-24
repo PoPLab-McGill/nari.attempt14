942ee8b4e11658208ba49eb3c6b280119bef8fed
Psychology of Pragmatics Lab <poplab.scsd@gmail.com>
2025-02-24 01:46:07 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -70,34 +70,34 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 					block : 1, //The block variable is not used later, but could help the user. 
 					//In each block, we can include a number of mini-blocks, to reduce repetition of same group/response.
 					miniBlocks : 2, //Set to 1 if don't need mini blocks. 0 will break the task.
-					singleAttTrials : 0, //Number of trials of the attribute that does not share key with the category (in a mini block).
-					sharedAttTrials : 24, //Number of trials of the attribute that shares key with the category (in a mini block).
-					categoryTrials :0 // Number of trials of the category (in a mini-block). If 0, the label does not appear.
+					singleAttTrials : 10, //Number of trials of the attribute that does not share key with the category (in a mini block).
+					sharedAttTrials : 7, //Number of trials of the attribute that shares key with the category (in a mini block).
+					categoryTrials : 7 // Number of trials of the category (in a mini-block). If 0, the label does not appear.
 					//Note: if no category trials, then attribute1, the one on the left, is considered the single attribute.
 				}, 
 				{ 
 					instHTML : '', 
 					block : 2, 
 					miniBlocks : 2, 
-					singleAttTrials : 0, 
-					sharedAttTrials : 36, 
-					categoryTrials : 0
+					singleAttTrials : 16, 
+					sharedAttTrials : 10, 
+					categoryTrials : 10
 				}, 
 				{ 
 					instHTML : '', 
 					block : 3, 
 					miniBlocks : 2, 
-					singleAttTrials : 0, 
-					sharedAttTrials : 24, 
-					categoryTrials : 0
+					singleAttTrials : 10, 
+					sharedAttTrials : 7, 
+					categoryTrials : 7
 				}, 
 				{ 
 					instHTML : '', 
 					block : 4, 
 					miniBlocks : 2, 
-					singleAttTrials : 0, 
-					sharedAttTrials : 36, 
-					categoryTrials : 0
+					singleAttTrials : 16, 
+					sharedAttTrials : 10, 
+					categoryTrials : 10
 				}, 
 			],
 			//All blocks show attribute1 on the left and attribute2 on the right. 
