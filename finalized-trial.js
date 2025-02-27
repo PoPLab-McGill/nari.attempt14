2ee4a91a4f2b1767c0191382211efcad58ea04f9
Psychology of Pragmatics Lab <poplab.scsd@gmail.com>
2025-02-27 22:50:23 +0100

    Update and rename updt-trial.js to finalized-trial.js

--- finalized-trial.js
+++ finalized-trial.js
@@ -2,15 +2,14 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/PoPLab-McGill/smiaa-sc-iat@main/u
 var API = new APIConstructor();
 		  return stiatExtension({
 		  category : { 
-		    name : 'Autistics', //Will appear in the data.
+		    name : 'Autistic', //Will appear in the data.
 		    title : {
-		      media : {word : 'Autistics'}, //Name of the category presented in the task.
+		      media : {word : 'Autistic'}, //Name of the category presented in the task.
 		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
 		      height : 7 //Used to position the "Or" in the combined block.
 		    }, 
             media: [
                     { word: 'Repeats body movement' },
-		    { word: 'Avoids eye contact' },
                     { word: 'Dislikes unexpected changes' },
                     { word: 'Struggles making friends' },
                     { word: 'Interprets language differently' }
