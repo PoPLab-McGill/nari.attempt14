bdde84fea2f42af474884e7f7e5b7786b723449f
Psychology of Pragmatics Lab <poplab.scsd@gmail.com>
2025-02-28 03:49:00 +0100

    Update and rename finalized-trial.js to trial.js

--- trial.js
+++ trial.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/PoPLab-McGill/smiaa-sc-iat@main/finalized-extension.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/PoPLab-McGill/qualtrics-smiaa-sc-iat@main/extension.js'], function(APIConstructor, stiatExtension){
 var API = new APIConstructor();
 		  return stiatExtension({
 		  category : { 
